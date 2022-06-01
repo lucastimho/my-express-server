67bf2fff8921aee36cbcd8e3544cab9e19bfb32a
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-31 20:26:27 -0500

    adds new route for server

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    response.send("<h1>Hello World!</h1>")
+    res.send("<h1>Hello World!</h1>")
 })
 
+app.get("/contact", function(req, res) {
+    res.send("Contact me at: test@example.com")
+})
 app.listen(3000, function() {
     console.log("Server started on port 3000")
 });
