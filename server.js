a7a1e04e149aa72ba5dbbebf2ac3e2ee6439fbbc
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-31 20:21:38 -0500

    tests request send

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(request, response) {
-    console.log(request);
+    request.send("Hello")
 })
 
 app.listen(3000, function() {
