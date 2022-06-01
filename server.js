1a5941b671188b1c5207d0289edf3f3af133bf4e
lucastimho <lucas.t.ho.lh@gmail.com>
2022-05-31 20:34:27 -0500

    creates about route

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get("/", function(req, res) {
 app.get("/contact", function(req, res) {
     res.send("Contact me at: test@example.com")
 })
+
+app.get("/about", function (req, res) {
+    res.send("My name is Lucas.")
+})
 app.listen(3000, function() {
     console.log("Server started on port 3000")
 });
